aac7c04bdc41e5fc74dc1c883a19ba56b09725a8
matiiezequiiel <matii.ezequiiel@gmail.com>
2020-09-10 20:14:53 -0300

    clase calculadora

---
+++ TP 1/Entidades/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    static class Calculadora
+    {
+        private static string ValidarOPerador(char operador)
+        {
+            return "algo";
+        }
+
+        public static double Operar (Numero num1,Numero num2,string operador)
+        {
+            return 0;
+        }
+
+    }
+}
